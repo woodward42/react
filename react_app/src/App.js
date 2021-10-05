0d314c45d7baef3bf3afaa2f276043043fe99556
woodward <andrey.ivanov.5891@gmail.com>
2021-10-05 10:49:34 +0300

    ДЗ 1 готово

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { Message } from './Message';
 
-function App() {
+const text = 'Текст для пропса в компонент Message';
+
+export function App() {
   return (
     <div className="App">
-      HELLO, WOODWARD
+      <h3 className="App-header"> homework-1</h3>
+      <Message text={text}/>
     </div>
   );
 }
 
-export default App;
+//export default App;
