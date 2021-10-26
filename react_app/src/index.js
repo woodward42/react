931fc7d589eda69939b6ac4c5b52f2cc2d42ed80
woodward <andrey.ivanov.5891@gmail.com>
2021-10-26 12:43:38 +0300

    Подключил персист, посмотрел сторадж, всё работает

--- react_app/src/index.js
+++ react_app/src/index.js
@@ -3,37 +3,37 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import { ChatPage, Profile } from "./pages";
-import { store } from "./store/create-store";
+import { store,persistor } from "./store/create-store";
 import { Provider } from "react-redux";
-import {Header} from './components'
+import { Header } from "./components";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
-  <Provider store={store}>
-	<BrowserRouter>
-      <Header />
-      <Switch>
-
-        <Route path="/chat">
+	<Provider store={store}>
+  <PersistGate persistor={persistor}>
+		<BrowserRouter>
+			<Header />
+			<Switch>
+					<ChatPage />
+          <Route path="/chat">
           <ChatPage />
         </Route>
 
-        <Route exact path="/profile">
-          <Profile />
-        </Route>
+				<Route exact path="/profile">
+					<Profile />
+				</Route>
 
-        <Route path="/">
-          <h1>Меню</h1>
-        </Route>
+				<Route path="/">
+					<h1>Меню</h1>
+				</Route>
 
-
-        <Route path="*">
-          <h1>404 страница не найдена</h1>
-          <Link to="/chat">Перейти в чат</Link>
-        </Route>
-
-      </Switch>
-  </BrowserRouter>
-  </Provider>,
+				<Route path="*">
+					<h1>404 страница не найдена</h1>
+					<Link to="/chat">Перейти в чат</Link>
+				</Route>
+			</Switch>
+		</BrowserRouter>
+    </PersistGate>
+	</Provider>,
 	document.getElementById("root")
 );
-
--- react_app/src/store/create-store.js
+++ react_app/src/store/create-store.js
@@ -4,15 +4,31 @@ import { ProfileReducer } from "./profile";
 import { conversationsReducer } from "./conversations";
 import { messagesReducer } from "./messages";
 import { BotSendAnswerMessage } from "./middlewares";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const pConfig = {
+	key: "react-app",
+	storage,
+	blacklist: ["messages"],
+	whitelist: ["profile", "conversations"],
+};
+
+const pReducer = persistReducer(
+    pConfig,
+    combineReducers({
+      profile: ProfileReducer,
+      conversations: conversationsReducer,
+      messages: messagesReducer,
+    })
+  );
 
 export const store = createStore(
-	combineReducers({
-		profile: ProfileReducer,
-		conversations: conversationsReducer,
-		messages: messagesReducer,
-	}),
+	pReducer,
 	compose(
-        applyMiddleware(thunk, BotSendAnswerMessage),
+		applyMiddleware(thunk, BotSendAnswerMessage)
 		//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	)
-)
+);
+
+export const persistor = persistStore(store);
